7e5b98c87de305cc184a7057445b4f8b29ba84eb
KirtiGhugtyal6 <93982610+KirtiGhugtyal6@users.noreply.github.com>
2023-01-29 00:25:36 +0530

    Update azureoperationsscript.ps1

--- azureoperationsscript.ps1
+++ azureoperationsscript.ps1
@@ -7,13 +7,8 @@ param (
 $orgUrl = $OrganizationUrl
 $pat = $PAT
 
-# Create header with PAT
-$token = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($pat)"))
-$header = @{authorization = "Basic $token"}
-
-
-# Get the list of all projects in the organization
 $projectsUrl = "$OrganizationUrl/_apis/projects?api-version=5.1"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $projects = Invoke-RestMethod -Uri $projectsUrl -Method Get -ContentType "application/json" -Headers $header
 $projects.value | ForEach-Object {
    Write-Host $_.id $_.name
@@ -21,4 +16,4 @@ $projects.value | ForEach-Object {
         'id' = $_.id
         'name' = $_.name
     }
-}    
+}
